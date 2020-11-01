c683c85003d788ec91129b0f533679c294efe4b8
Justin <justin.antunes-cardoso@hotmail.com>
2020-11-01 14:23:11 -0500

    first commit

---
+++ app/src/main/java/com/mycompany/squadz/Intro_1_1.java
@@ -0,0 +1,14 @@
+package com.mycompany.squadz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intro_1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_1_1);
+    }
+}
---
+++ app/src/main/java/com/mycompany/squadz/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mycompany.squadz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //txt view
+        TextView txtView = (TextView) findViewById(R.id.text_id);
+    }
+}
